76ac74b5a7c6f44f494abac77d9688dc7df37443
Coilin P. Boylan Jeritslev <coilinpbj@gmail.com>
2020-05-30 11:16:25 +0200

    Improve the script of the bar graph in the realtime webcam show

--- webcam_machine_learning.py
+++ webcam_machine_learning.py
@@ -1,4 +1,5 @@
 ## Importing the required module 
+import numpy                    # For matrix
 import cv2                      # For webcam
 import matplotlib.pyplot as plt # For plot
 
@@ -13,34 +14,59 @@ else:
 
 ## Analysis - Initialized part
 
-# x-coordinates of left sides of bars  
-left = [1, 2, 3, 4, 5] 
-  
-# heights of bars 
-height = [10, 24, 36, 40, 5] 
-  
-# labels for bars 
-tick_label = ['one', 'two', 'three', 'four', 'five'] 
-  
-# plotting a bar chart 
-plt.bar(left, height, tick_label = tick_label, 
-        width = 0.8, color = ['red', 'green']) 
-  
-# naming the x-axis 
-plt.xlabel('x - axis') 
-# naming the y-axis 
-plt.ylabel('y - axis') 
-# plot title 
-plt.title('My bar chart!') 
+color = 'rgb'
 
+# Initialize plot.
+fig, ax = plt.subplots()
+if color == 'rgb':
+    ax.set_title('Histogram (RGB)')
+else:
+    ax.set_title('Histogram (grayscale)')
+ax.set_xlabel('Bin')
+ax.set_ylabel('Frequency')
+
+# Initialize plot line object(s). Turn on interactive plotting and show plot.
+lw = 3
+alpha = 0.5
+if color == 'rgb':
+    lineR, = ax.plot(np.arange(bins), np.zeros((bins,)), c='r', lw=lw, alpha=alpha)
+    lineG, = ax.plot(np.arange(bins), np.zeros((bins,)), c='g', lw=lw, alpha=alpha)
+    lineB, = ax.plot(np.arange(bins), np.zeros((bins,)), c='b', lw=lw, alpha=alpha)
+else:
+    lineGray, = ax.plot(np.arange(bins), np.zeros((bins,1)), c='k', lw=lw)
+ax.set_xlim(0, bins-1)
+ax.set_ylim(0, 1)
+plt.ion()
 plt.show()
 
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()    
 
+    # Normalize histograms based on number of pixels per frame.
+    numPixels = np.prod(frame.shape[:2])
+    if color == 'rgb':
+        cv2.imshow('RGB', frame)
+        (b, g, r) = cv2.split(frame)
+        histogramR = cv2.calcHist([r], [0], None, [bins], [0, 255]) / numPixels
+        histogramG = cv2.calcHist([g], [0], None, [bins], [0, 255]) / numPixels
+        histogramB = cv2.calcHist([b], [0], None, [bins], [0, 255]) / numPixels
+        lineR.set_ydata(histogramR)
+        lineG.set_ydata(histogramG)
+        lineB.set_ydata(histogramB)
+    else:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Grayscale', gray)
+        histogram = cv2.calcHist([gray], [0], None, [bins], [0, 255]) / numPixels
+        lineGray.set_ydata(histogram)
+    
+    fig.canvas.draw()
+
     key = cv2.waitKey(20)
 
     if key == 27: # exit on ESC
         break
-cv2.destroyWindow("preview")
+
+capture.release()    
+#cv2.destroyWindow("preview")
+cv2.destroyAllWindows()
